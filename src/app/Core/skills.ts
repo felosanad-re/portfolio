27a1e7e1e03dbeb0f2647f15603997b8efd09cd9
felosanad <felosanad@gmail.com>
2026-04-18 18:54:33 +0200

    add skills interface in about section

---
+++ src/app/Core/skills.ts
@@ -0,0 +1,4 @@
+export interface Skills {
+  name: string;
+  percentage?: number | null;
+}
--- src/app/Pages/about/about.component.ts
+++ src/app/Pages/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Skills } from '../../Core/skills';
 
 @Component({
   selector: 'app-about',
@@ -8,6 +9,32 @@ import { Component } from '@angular/core';
   styleUrl: './about.component.scss',
 })
 export class AboutComponent {
+  frontEndSkills!: Skills[];
+  backEndSkills!: Skills[];
+  toolsSkills!: Skills[];
+
+  ngOnInit() {
+    this.frontEndSkills = [
+      { name: 'html', percentage: 95 },
+      { name: 'css', percentage: 95 },
+      { name: 'javascript', percentage: 85 },
+      { name: 'angular', percentage: 75 },
+    ];
+
+    this.backEndSkills = [
+      { name: '.Net framework', percentage: 75 },
+      { name: 'C#', percentage: 80 },
+      { name: 'SQL Database', percentage: 80 },
+      { name: 'Entity Framework', percentage: 80 },
+    ];
+
+    this.toolsSkills = [
+      { name: 'Git', percentage: 80 },
+      { name: 'Postman', percentage: 85 },
+      { name: 'REST APIs', percentage: 85 },
+    ];
+  }
+
   getSectionHeight() {
     const section = document.getElementById('about');
     if (section) {
